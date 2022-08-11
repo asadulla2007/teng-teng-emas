4e0d2e4a08456596cdbbc0bfd44590a7cf73a763
asadulla2007 <110012942+asadulla2007@users.noreply.github.com>
2022-08-11 01:03:57 -0400

    Add files via upload

---
+++ untitled24/src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Age write :  ");
+//        int age = scanner.nextInt();
+//        if (age >= 18) {
+//            System.out.println("Welcome");
+//        }else {
+//            System.out.println("Error");
+
+
+//        System.out.print("a = ");
+//        int a =scanner .nextInt() ;
+//        System.out.print("b = ");
+//        int b =scanner .nextInt() ;
+//
+//
+//
+//        if (a<b) {
+//
+//            System.out.println("Min " + a);
+//        }else {
+//            System.out.println("Min " +b);
+
+
+//        System.out.print("Number  write :");
+//
+//        int number = scanner .nextInt();
+//        if (number%2==0) {
+//            System.out.println("Juft");
+//        }else {
+//            System.out.println("Toq");
+        System.out.print("a = ");
+        int a =scanner.nextInt() ;
+        System.out.print("b= ");
+        int b = scanner.nextInt() ;
+        if (a==b) {
+            System.out.println("Teng");
+        }else {
+            System.out.println("Tengmas");
+
+        }
+    }
+}
